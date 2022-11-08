3bbba15f75ab304dbc78db6eeadd2a58810d683f
ethramos22 <ethramos22@gmail.com>
2022-11-08 09:29:37 -0700

    Displaying basic movie poster data

--- main/js/detailCard.js
+++ main/js/detailCard.js
@@ -5,8 +5,7 @@ class MovieDetailCard {
     constructor(globalMovieData) {
         this.globalMovieData = globalMovieData;
         console.log('Start Constructor of Movie Detail Card', this.globalMovieData);
-        // Testing purposes, remove with interaction
-        this.globalMovieData.selectedMovie = this.globalMovieData.popularMovies[0];
+        
         console.log('selected movie: ', this.globalMovieData.selectedMovie);
         
         this.drawPoster();
@@ -35,14 +34,20 @@ class MovieDetailCard {
             .text('$'+d.revenue);
         
         let body = selection.append('div')
-            .attr('id', 'poster-body');
-        body.append('div')
-            .attr('id', 'poster-overview')
-            .text(d.overview);
+            .attr('id', 'poster-body')
+            .classed('float-container', true);
         body.append('svg')
             .attr('id', 'poster-image')
+            .classed('float-child', true)
+            .attr('width', 500)
+            .attr('height', 800)
             .append('image')
             .attr('href', base_image_url + d.poster_path);
+        body.append('div')
+            .attr('id', 'poster-overview')
+            .classed('float-child', true)
+            .text(d.overview);
+       
         
         let footing = selection.append('div')
             .attr('id', 'poster-footing');
