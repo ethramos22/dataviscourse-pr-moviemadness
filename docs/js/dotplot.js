b0a52d0b615e4334a4d4a03cf7bd678a401edacd
Kaden Hendrickson <kadenhendrickson4@gmail.com>
2022-11-18 11:34:50 -0700

    dotplot tooltil displays movie name and respective units on hover

--- docs/js/dotplot.js
+++ docs/js/dotplot.js
@@ -29,9 +29,9 @@ class Dotplot {
             .range([this.CHART_HEIGHT - this.MARGIN.bottom, this.MARGIN.top])
 
         // position axis
-        d3.select('#bvrev-x-axis')
+        d3.select('#dotplot-x-axis')
             .attr('transform', `translate(0, ${this.CHART_HEIGHT - this.MARGIN.bottom})`)
-        d3.select('#bvrev-y-axis')
+        d3.select('#dotplot-y-axis')
             .attr('transform', `translate(${this.MARGIN.left}, 0)`)
 
         this.drawChart();
@@ -67,7 +67,7 @@ class Dotplot {
                     return d;
                 return d/1000000;
             });
-        d3.select('#bvrev-x-axis').call(xAxis);
+        d3.select('#dotplot-x-axis').call(xAxis);
 
         // draw yAxis
         let yAxis = d3.axisLeft()
@@ -78,10 +78,10 @@ class Dotplot {
                 return d/1000000;
             });
 
-        d3.select('#bvrev-y-axis').call(yAxis);
+        d3.select('#dotplot-y-axis').call(yAxis);
 
         //TODO: FORMAT AXIS AND DRAW LABELS
-        d3.select('#bvrev-x-axis').selectAll('.tick text')
+        d3.select('#dotplot-x-axis').selectAll('.tick text')
             .attr("y", 0)
             .attr("x", 9)
             .attr("dy", ".35em")
@@ -92,16 +92,82 @@ class Dotplot {
     }
 
     drawCircles() {
-        let circleSelection = d3.select('#bvrev-content')
+        let _this = this;
+
+        let circleSelection = d3.select('#dotplot-content')
             .selectAll('circle')
             .data(this.movieData)
             .join('circle')
+            .on('mouseover', function(event, d ) {
+                console.log('in highlight selected:', event, d, this)
+        
+                // Get x and y position of circle
+                const cx = parseFloat(d3.select(this).attr('cx'));
+                const cy = parseFloat(d3.select(this).attr('cy'));
+                // Adjust x and y position of tooltip based on position of circle
+                let tooltip = d3.select('#dotplot-tooltip');
+                let tooltipEnter = tooltip.selectAll('text')
+                    .data([d])
+                    .enter();
+
+                tooltipEnter.append('text')
+                    .text(d => d.title)
+                    .attr('x', cx)
+                    .attr('y', d => cy > 300 ? cy - 50 : cy + 30)
+                    .attr('class', 'tooltip-label')
+                    .attr('text-anchor', d => cx < 250 ? 'start' : 'end');
+
+                // y axis info
+                tooltipEnter.append('text')
+                    .text(d => {
+                        let label = _this.yAxisData.text + ': ';
+
+                        if(_this.yAxisData.key === 'runtime')
+                            return label + d[_this.yAxisData.key] + ' minutes'
+
+                        if(_this.yAxisData.key == 'vote_average')
+                            return label + d[_this.yAxisData.key]
+
+
+                        return label + d3.format("$,")(d[_this.yAxisData.key]);
+                    })
+                    .attr('x', cx)
+                    .attr('y', d => cy > 300 ? cy - 35 : cy + 45)
+                    .attr('class', 'tooltip-label')
+                    .attr('text-anchor', d => cx < 250 ? 'start' : 'end');
+
+                // X axis info
+                tooltipEnter.append('text')
+                    .text(d => {
+                        let label = _this.xAxisData.text + ': ';
+
+                        if(_this.xAxisData.key === 'runtime')
+                            return label + d[_this.xAxisData.key] + ' minutes'
+
+                        if(_this.xAxisData.key == 'vote_average')
+                            return label + d[_this.xAxisData.key]
+
+
+                        return label + d3.format("$,")(d[_this.xAxisData.key]);
+                    })
+                    .attr('x', cx)
+                    .attr('y', d => cy > 300 ? cy - 20 : cy + 60)
+                    .attr('class', 'tooltip-label')
+                    .attr('text-anchor', d => cx < 250 ? 'start' : 'end');
+            })
+            .on('mouseout', (event, d) => {
+                d3.select('#dotplot-tooltip')
+                    .selectAll('text')
+                    .remove();
+                // d3.select('#dotplot-tooltip')
+                //     .selectAll('rect')
+                //     .remove();
+            })
             .transition().duration(300)
             .attr('cx', d => this.xScale(d[this.xAxisData.key]))
             .attr('cy', d => this.yScale(d[this.yAxisData.key]))
-            .attr('r', 3)
-            .attr('stroke', 'black')
-            .attr('fill', 'red');
+            .attr('r', 5)
+            .attr('class', 'movie-dot');
     }
 
     drawLabelsAndTitles() {
@@ -109,7 +175,7 @@ class Dotplot {
         // change .data binding to be information about what the label says
 
         // Y axis label
-        d3.select('#dot-plot-labelY').selectAll('text')
+        d3.select('#dotplot-labelY').selectAll('text')
             .data([this.yAxisData])
             .join('text')
             .text(d => {
@@ -125,7 +191,7 @@ class Dotplot {
             .attr('transform', `translate(20, ${this.yAxisData.key === 'vote_average' ? this.CHART_HEIGHT/2 :this.CHART_HEIGHT/2 + this.MARGIN.top + 25}) rotate(-90)`);
 
         // X axis label
-        d3.select('#dot-plot-labelX').selectAll('text')
+        d3.select('#dotplot-labelX').selectAll('text')
             .data([this.xAxisData])
             .join('text')       
             .text(d => {
@@ -140,12 +206,12 @@ class Dotplot {
             .attr('fill', 'white');
         
         // Chart title
-        d3.select('#dot-plot-title').selectAll('text')
+        d3.select('#dotplot-title').selectAll('text')
             .data([this.movieData])
             .join('text')       
             .text(d => this.chartTitle)
             .attr('x', this.CHART_WIDTH/2 - 25)
-            .attr('y', 30)
+            .attr('y', 15)
             .attr('fill', 'white')
             .attr('font-size', 18);
     }
@@ -184,5 +250,4 @@ class Dotplot {
         this.chartTitle = this.yAxisData.text + ' vs ' + this.xAxisData.text;
         this.drawChart();
     }
-
 }
